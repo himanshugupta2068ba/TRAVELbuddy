cfefffe88096a4397df2a009aa897a621a6d4c70
Himanshu Gupta <himanshugupta2068ba@gmail.com>
2025-09-04 19:29:44 +0530

    backend and frontend connect

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,14 @@
-import express from "express";
+const express = require("express");
 const app = express();
-app.set("port",(process.env.port || 8000));
+const cors = require("cors");
 
+app.use(cors()); // 👈 allows frontend requests
+app.use(express.json());
 
-app.listen(app.get("port"),()=>{
-    console.log(`Server started on port ${app.get("port")}`);
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from backend 🚀" });
+});
+
+app.listen(5000, () => {
+    console.log("Backend running on http://localhost:5000");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import SetupExample from './setupexample.jsx'
 
 function App() {
 
   return (
     <>
-      <h1>first change by addi</h1>
+     <SetupExample />
     </>
   )
 }
